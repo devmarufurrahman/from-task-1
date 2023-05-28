c2aeebe12605b311b641c506a2cb9eb20583c1ae
marufur-rahman <mdmarufur5682@gmail.com>
2023-05-28 12:30:44 +0600

    all design complete

--- src/Components/FormData/index.jsx
+++ src/Components/FormData/index.jsx
@@ -1,5 +1,127 @@
+import { GrLocation } from "react-icons/gr";
+import { BiUserCircle, BiBriefcase } from "react-icons/bi";
+
 const FormData = () => {
-	return <div>FormData</div>;
+	return (
+		<>
+			<div className="mt-[160px] mx-4 md:mt-[160px] relative">
+				<div className="bg-gray-100 rounded-md max-w-6xl w-full mx-auto">
+					<form className="first-hero lws-inputform  ">
+						{/* <!-- From --> */}
+						<div className="des-from">
+							<p>Destination From</p>
+							<div className="flex flex-row items-center">
+								<GrLocation />
+								<select
+									className="outline-none bg-gray-100 px-2 py-2 w-full"
+									name="from"
+									id="lws-from"
+									required>
+									<option value="" hidden>
+										Please Select
+									</option>
+									<option>Dhaka</option>
+									<option>Sylhet</option>
+									<option>Saidpur</option>
+									<option>Coxs Bazar</option>
+								</select>
+							</div>
+						</div>
+
+						{/* <!-- To --> */}
+						<div className="des-from">
+							<p>Destination To</p>
+							<div className="flex flex-row items-center">
+								<GrLocation />
+								<select
+									className="outline-none bg-gray-100 px-2 py-2 w-full"
+									name="to"
+									id="lws-to"
+									required>
+									<option value="" hidden>
+										Please Select
+									</option>
+									<option>Dhaka</option>
+									<option>Sylhet</option>
+									<option>Saidpur</option>
+									<option>Coxs Bazar</option>
+								</select>
+							</div>
+						</div>
+
+						{/* <!-- Date --> */}
+						<div className="des-from">
+							<p>Journey Date</p>
+							<input
+								type="date"
+								className="outline-none bg-gray-100 px-2 py-2 w-full date"
+								name="date"
+								id="lws-date"
+								required
+							/>
+						</div>
+
+						{/* <!-- Guests --> */}
+						<div className="des-from">
+							<p>Guests</p>
+							<div className="flex flex-row items-center">
+								<BiUserCircle />
+								<select
+									className="outline-none bg-gray-100 px-2 py-2 w-full"
+									name="guests"
+									id="lws-guests"
+									required>
+									<option value="" hidden>
+										Please Select
+									</option>
+									<option value="1">1 Person</option>
+									<option value="2">2 Persons</option>
+									<option value="3">3 Persons</option>
+									<option value="4">4 Persons</option>
+								</select>
+							</div>
+						</div>
+
+						{/* <!-- className --> */}
+						<div className="des-from !border-r-0">
+							<p>Class</p>
+							<div className="flex flex-row items-center">
+								<BiBriefcase />
+								<select
+									className="outline-none bg-gray-100 px-2 py-2 w-full"
+									name="ticketclassName"
+									id="lws-ticketclassName"
+									required>
+									<option value="" hidden>
+										Please Select
+									</option>
+									<option>Business</option>
+									<option>Economy</option>
+								</select>
+							</div>
+						</div>
+
+						<button className="addCity" type="submit" id="lws-addCity">
+							<svg
+								width="15px"
+								height="15px"
+								fill="none"
+								viewBox="0 0 24 24"
+								strokeWidth="2"
+								stroke="currentColor">
+								<path
+									strokeLinecap="round"
+									strokeLinejoin="round"
+									d="M12 4.5v15m7.5-7.5h-15"
+								/>
+							</svg>
+							<span className="text-sm">Book</span>
+						</button>
+					</form>
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default FormData;
--- src/Components/TableData/index.jsx
+++ src/Components/TableData/index.jsx
@@ -1,5 +1,114 @@
+import { AiOutlineDelete } from "react-icons/ai";
+
 const TableData = () => {
-	return <div>TableData</div>;
+	return (
+		<>
+			<div>
+				{/* <!-- Preview Data --> */}
+				<div className="table-container">
+					<table className="booking-table">
+						<thead className="bg-gray-100/50">
+							<tr className="text-black text-left">
+								<th>Destination From</th>
+								<th>Destination To</th>
+								<th className="text-center">Journey Date</th>
+								<th className="text-center">Guests</th>
+								<th className="text-center">className</th>
+								<th className="text-center">Delete</th>
+							</tr>
+						</thead>
+						<tbody
+							className="divide-y divide-gray-300/20"
+							id="lws-previewBooked">
+							{/* <!-- Row 1 --> */}
+							<tr className="lws-bookedTable text-black">
+								<td className="px-6 py-4">
+									<div className="flex items-center space-x-3">
+										<p className="lws-bookedFrom">Dhaka</p>
+									</div>
+								</td>
+								<td className="px-6 py-4">
+									<p className="lws-bookedTo">Sylhet</p>
+								</td>
+								<td className="px-6 py-4 text-center">
+									<p className="lws-bookedDate">11-01-23</p>
+								</td>
+								<td className="px-6 py-4 text-center">
+									<p className="lws-bookedGustes">2</p>
+								</td>
+								<td className="px-6 py-4 text-center">
+									<span className="lws-bookedclassName"> Business </span>
+								</td>
+								<td className="px-6 py-4 text-center">
+									<div className="flex justify-center gap-4">
+										<button className="lws-remove">
+											<AiOutlineDelete className="bg-red-100 border-2 border-red-700 w-8 h-8 " />
+										</button>
+									</div>
+								</td>
+							</tr>
+
+							{/* <!-- Row 2 --> */}
+							<tr className="lws-bookedTable text-black">
+								<td className="px-6 py-4">
+									<div className="flex items-center space-x-3">
+										<p className="lws-bookedFrom">Dhaka</p>
+									</div>
+								</td>
+								<td className="px-6 py-4">
+									<p className="lws-bookedTo">Sylhet</p>
+								</td>
+								<td className="px-6 py-4 text-center">
+									<p className="lws-bookedDate">11-01-23</p>
+								</td>
+								<td className="px-6 py-4 text-center">
+									<p className="lws-bookedGustes">2</p>
+								</td>
+								<td className="px-6 py-4 text-center">
+									<span className="lws-bookedclassName"> Business </span>
+								</td>
+								<td className="px-6 py-4 text-center">
+									<div className="flex justify-center gap-4">
+										<button className="lws-remove">
+											<AiOutlineDelete className="bg-red-100 border-2 border-red-700 w-8 h-8 " />
+										</button>
+									</div>
+								</td>
+							</tr>
+
+							{/* <!-- Row 3 --> */}
+							<tr className="lws-bookedTable text-black">
+								<td className="px-6 py-4">
+									<div className="flex items-center space-x-3">
+										<p className="lws-bookedFrom">Dhaka</p>
+									</div>
+								</td>
+								<td className="px-6 py-4">
+									<p className="lws-bookedTo">Sylhet</p>
+								</td>
+								<td className="px-6 py-4 text-center">
+									<p className="lws-bookedDate">11-01-23</p>
+								</td>
+								<td className="px-6 py-4 text-center">
+									<p className="lws-bookedGustes">2</p>
+								</td>
+								<td className="px-6 py-4 text-center">
+									<span className="lws-bookedclassName"> Business </span>
+								</td>
+								<td className="px-6 py-4 text-center">
+									<div className="flex justify-center gap-4">
+										<button className="lws-remove">
+											<AiOutlineDelete className="bg-red-100 border-2 border-red-700 w-8 h-8 " />
+										</button>
+									</div>
+								</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default TableData;
